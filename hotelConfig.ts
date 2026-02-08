568482f5d8b785ea86b13fe10abdf1d8129c7d07
frontend-alpinads <fath@alpinads.com>
2026-02-08 15:47:40 +0100

    Populate hotelConfig with scraped data

--- hotelConfig.ts
+++ hotelConfig.ts
@@ -1,7 +1,7 @@
 export const hotelConfig = {
-  hotelName: "{{HOTEL_NAME}}",
-  email: "{{EMAIL}}",
-  address: "{{ADDRESS}}",
-  phoneNumber: "{{PHONE_NUMBER}}",
-  websiteUrl: "{{WEBSITE_URL}}",
+  hotelName: "Tratterhof",
+  email: "",
+  address: "",
+  phoneNumber: "",
+  websiteUrl: "https://www.tratterhof.com/",
 };
